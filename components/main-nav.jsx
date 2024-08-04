9e5e9785193ba435291c5ea7282b24227dcbc913
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2024-08-04 14:40:41 +0600

    login-session

--- components/main-nav.jsx
+++ components/main-nav.jsx
@@ -6,6 +6,8 @@ import { useState } from "react";
 
 import { MobileNav } from "@/components/mobile-nav";
 import { Menu, X } from "lucide-react";
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
 import { Logo } from "./logo";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Button, buttonVariants } from "./ui/button";
@@ -15,8 +17,19 @@ import {
 	DropdownMenuItem,
 	DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
+
+
+
+
 export function MainNav({ items, children }) {
+	const { data: session } = useSession();
 	const [showMobileMenu, setShowMobileMenu] = useState(false);
+	const [loginSession, setLoginSession] = useState(null);
+
+	useEffect(() => {
+		console.log("test");
+		setLoginSession(session);
+	}, [session]);
 
 	return (
 		<>
@@ -44,28 +57,41 @@ export function MainNav({ items, children }) {
 				)}
 			</div>
 			<nav className="flex items-center gap-3">
-				<div className="items-center gap-3 hidden lg:flex">
-					<Link
-						href="/login"
-						className={cn(buttonVariants({ size: "sm" }), "px-4")}>
-						Login
-					</Link>
-					<DropdownMenu>
-						<DropdownMenuTrigger asChild>
-							<Button variant="outline" size="sm">
-								Register
-							</Button>
-						</DropdownMenuTrigger>
-						<DropdownMenuContent align="end" className="w-56 mt-4">
-							<DropdownMenuItem className="cursor-pointer">
-								<Link href="/register/student">Student</Link>
-							</DropdownMenuItem>
-							<DropdownMenuItem className="cursor-pointer">
-								<Link href="/register/instructor">Instructor</Link>
-							</DropdownMenuItem>
-						</DropdownMenuContent>
-					</DropdownMenu>
-				</div>
+				{!loginSession && (
+					<div className="items-center gap-3 hidden lg:flex">
+						<Link
+							href="/login"
+							className={cn(
+								buttonVariants({ size: "sm" }),
+								"px-4"
+							)}
+						>
+							Login
+						</Link>
+						<DropdownMenu>
+							<DropdownMenuTrigger asChild>
+								<Button variant="outline" size="sm">
+									Register
+								</Button>
+							</DropdownMenuTrigger>
+							<DropdownMenuContent
+								align="end"
+								className="w-56 mt-4"
+							>
+								<DropdownMenuItem className="cursor-pointer">
+									<Link href="/register/student">
+										Student
+									</Link>
+								</DropdownMenuItem>
+								<DropdownMenuItem className="cursor-pointer">
+									<Link href="/register/instructor">
+										Instructor
+									</Link>
+								</DropdownMenuItem>
+							</DropdownMenuContent>
+						</DropdownMenu>
+					</div>
+				)}
 				<DropdownMenu>
 					<DropdownMenuTrigger asChild>
 						<div className="cursor-pointer">
