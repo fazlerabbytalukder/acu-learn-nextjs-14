56afcd2f0f5e5e9ebc71589aaa5ce5c1e64a4466
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2024-08-04 14:49:42 +0600

    signout

--- components/main-nav.jsx
+++ components/main-nav.jsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 
 import { MobileNav } from "@/components/mobile-nav";
 import { Menu, X } from "lucide-react";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import { useEffect } from "react";
 import { Logo } from "./logo";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
@@ -23,8 +23,11 @@ import {
 
 export function MainNav({ items, children }) {
 	const { data: session } = useSession();
+
 	const [showMobileMenu, setShowMobileMenu] = useState(false);
 	const [loginSession, setLoginSession] = useState(null);
+
+	console.log(loginSession);
 
 	useEffect(() => {
 		console.log("test");
@@ -45,7 +48,8 @@ export function MainNav({ items, children }) {
 								href={item.disabled ? "#" : item.href}
 								className={cn(
 									"flex items-center text-lg font-medium transition-colors hover:text-foreground/80 sm:text-sm"
-								)}>
+								)}
+							>
 								{item.title}
 							</Link>
 						))}
@@ -109,19 +113,22 @@ export function MainNav({ items, children }) {
 							<Link href="account">Profile</Link>
 						</DropdownMenuItem>
 						<DropdownMenuItem className="cursor-pointer" asChild>
-							<Link href="account/enrolled-courses">My Courses</Link>
+							<Link href="account/enrolled-courses">
+								My Courses
+							</Link>
 						</DropdownMenuItem>
 						<DropdownMenuItem className="cursor-pointer" asChild>
 							<Link href="">Testimonials & Certificates</Link>
 						</DropdownMenuItem>
 						<DropdownMenuItem className="cursor-pointer" asChild>
-							<Link href="">Logout</Link>
+							<Link href="#" onClick={() => { signOut() }}>Logout</Link>
 						</DropdownMenuItem>
 					</DropdownMenuContent>
 				</DropdownMenu>
 				<button
 					className="flex items-center space-x-2 lg:hidden"
-					onClick={() => setShowMobileMenu(!showMobileMenu)}>
+					onClick={() => setShowMobileMenu(!showMobileMenu)}
+				>
 					{showMobileMenu ? <X /> : <Menu />}
 				</button>
 			</nav>
