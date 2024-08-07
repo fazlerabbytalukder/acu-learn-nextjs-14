29a9e54b2cc974f8eb4def0659a470d1a7087ca0
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2024-08-08 03:03:56 +0600

    success message stripe payment

--- app/(main)/enroll-success/page.jsx
+++ app/(main)/enroll-success/page.jsx
@@ -1,15 +1,66 @@
+import { auth } from "@/auth";
 import { Button } from "@/components/ui/button";
+import { stripe } from "@/lib/stripe";
+import { getCourseDetails } from "@/queries/courses";
+import { getUserByEmail } from "@/queries/users";
 import { CircleCheck } from "lucide-react";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
-const Success = () => {
+const Success = async ({ searchParams: { session_id, courseId } }) => {
+  console.log(session_id, courseId);
+
+  if (!session_id)
+    throw new Error(
+      "Please provide a valid session id that starts with cs_"
+    );
+
+  const userSession = await auth();
+  // console.log(userSession);
+
+
+  if (!userSession?.user?.email) {
+    redirect("/login");
+  }
+
+  const course = await getCourseDetails(courseId);
+  const loggedInUser = await getUserByEmail(userSession?.user?.email);
+
+  const checkoutSession = await stripe.checkout.sessions.retrieve(
+    session_id,
+    {
+      expand: ["line_items", "payment_intent"],
+    }
+  );
+
+  // console.log(checkoutSession);
+
+  const paymentIntent = checkoutSession?.payment_intent;
+  const paymentStatus = paymentIntent?.status;
+
+  // console.log(paymentStatus);
+
+  // Cutomer info
+  const customerName = `${loggedInUser?.firstName} ${loggedInUser?.lastName}`;
+  const customerEmail = loggedInUser?.email;
+  const productName = course?.title;
+  console.log(productName, customerName, customerEmail);
+
+  if (paymentStatus === "succeeded") {
+
+  }
+
+
+
   return (
     <div className="h-full w-full flex-1 flex flex-col items-center justify-center">
       <div className="flex flex-col items-center gap-6 max-w-[600px] text-center">
-        <CircleCheck className="w-32 h-32 bg-success rounded-full p-0 text-green-600" />
-        <h1 className="text-xl md:text-2xl lg:text-3xl">
-          Congratulations! You Enrollment was Successful
-        </h1>
+        {paymentStatus === "succeeded" && (<>
+          <CircleCheck className="w-32 h-32 bg-success rounded-full p-0 text-green-600" />
+          <h1 className="text-xl md:text-2xl lg:text-3xl">
+            Congratulations, <strong>{customerName}</strong>! Your Enrollment was Successful for <strong>{productName}</strong>
+          </h1>
+        </>)}
         <div className="flex items-center gap-3">
           <Button asChild size="sm">
             <Link href="/courses">Browse Courses</Link>
