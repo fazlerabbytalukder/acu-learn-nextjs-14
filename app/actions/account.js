f1cf04b903f9ede39db74c6eb7b6f0da4433fb95
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2024-08-08 18:31:28 +0600

    personal details cash remove

--- app/actions/account.js
+++ app/actions/account.js
@@ -1,13 +1,13 @@
 "use server"
 
 import { User } from "@/model/user-model";
-// import { revalidatePath } from "next/cache";
+import { revalidatePath } from "next/cache";
 
 export async function updateUserInfo(email, updatedData) {
     try {
         const filter = { email: email };
         await User.findOneAndUpdate(filter, updatedData);
-        // revalidatePath('/account');
+        revalidatePath('/account');
     } catch (error) {
         throw new Error(error)
     }
