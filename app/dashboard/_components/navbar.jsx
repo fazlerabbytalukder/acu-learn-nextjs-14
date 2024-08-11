4cdad59214adc7b0bb29ae2dbd6c7be17d68779b
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2024-08-11 23:52:54 +0600

    dashboard navbar logout and avater

--- app/dashboard/_components/navbar.jsx
+++ app/dashboard/_components/navbar.jsx
@@ -1,17 +1,35 @@
 "use client";
 
-import { Logo } from "@/components/logo";
-import { MobileSidebar } from "./mobile-sidebar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Link from "next/link";
+import { MobileSidebar } from "./mobile-sidebar";
+
+import { signOut } from "next-auth/react";
+import { useEffect, useState } from "react";
 
 export const Navbar = () => {
+  const [loggedInUser, setLoggedInUser] = useState(null);
+
+  useEffect(() => {
+    async function fetchMe() {
+      try {
+        const response = await fetch(`/api/me`);
+        const data = await response.json();
+        setLoggedInUser(data);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    fetchMe();
+  }, []);
+
+
   return (
     <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
       <MobileSidebar />
@@ -21,7 +39,7 @@ export const Navbar = () => {
             <div className="cursor-pointer">
               <Avatar>
                 <AvatarImage
-                  src="https://github.com/shadcn.png"
+                  src={loggedInUser?.profilePicture}
                   alt="@shadcn"
                 />
                 <AvatarFallback>CN</AvatarFallback>
@@ -30,13 +48,12 @@ export const Navbar = () => {
           </DropdownMenuTrigger>
           <DropdownMenuContent align="end" className="w-56 mt-4">
             <DropdownMenuItem className="cursor-pointer">
-              <Link href="">Item One</Link>
-            </DropdownMenuItem>
-            <DropdownMenuItem className="cursor-pointer">
-              <Link href="">Item Two</Link>
-            </DropdownMenuItem>
-            <DropdownMenuItem className="cursor-pointer">
-              <Link href="">Logout</Link>
+              <Link
+                href="#"
+                onClick={() => {
+                  signOut();
+                }}
+              >Logout</Link>
             </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
