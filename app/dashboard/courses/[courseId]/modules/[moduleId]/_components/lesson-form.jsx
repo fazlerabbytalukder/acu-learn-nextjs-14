0f8eb1969c5c4f07431adc69dd91571896141b5f
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2024-08-14 22:12:31 +0600

    lesson title edit

--- app/dashboard/courses/[courseId]/modules/[moduleId]/_components/lesson-form.jsx
+++ app/dashboard/courses/[courseId]/modules/[moduleId]/_components/lesson-form.jsx
@@ -27,12 +27,14 @@ const formSchema = z.object({
   title: z.string().min(1),
 });
 
-export const LessonForm = ({ initialData, moduleId }) => {
+export const LessonForm = ({ initialData, moduleId, courseId }) => {
   const [isEditing, setIsEditing] = useState(false);
   const [lessons, setLessons] = useState(initialData);
   const router = useRouter();
   const [isCreating, setIsCreating] = useState(false);
   const [isUpdating, setIsUpdating] = useState(false);
+
+  const [lessonToEdit, setLessonToEdit] = useState(null);
 
   const toggleCreating = () => setIsCreating((current) => !current);
   const toggleEditing = () => setIsEditing((current) => !current);
@@ -86,6 +88,8 @@ export const LessonForm = ({ initialData, moduleId }) => {
   };
 
   const onEdit = (id) => {
+    const foundLesson = lessons.find(lesson => lesson.id === id);
+    setLessonToEdit(foundLesson);
     setIsEditing(true);
   };
 
@@ -158,7 +162,7 @@ export const LessonForm = ({ initialData, moduleId }) => {
           Drag & Drop to reorder the lessons
         </p>
       )}
-      <LessonModal open={isEditing} setOpen={setIsEditing} />
+      <LessonModal open={isEditing} setOpen={setIsEditing} courseId={courseId} lesson={lessonToEdit} />
     </div>
   );
 };
