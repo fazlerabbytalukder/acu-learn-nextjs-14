c45f1a7d3498ccc8029dccacb5ea5c9c16d85d65
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2024-08-15 23:00:21 +0600

    quiz set show

--- queries/quizzes.js
+++ queries/quizzes.js
@@ -1,15 +1,24 @@
-import { replaceMongoIdInArray } from "@/lib/convertData";
+import { replaceMongoIdInArray, replaceMongoIdInObject } from "@/lib/convertData";
 import { Quizset } from "@/model/quizset-model";
 import { Quiz } from "@/model/quizzes-model";
 
 export async function getAllQuizSets(excludeUnPublished) {
     try {
-        const quizSets = await Quizset.find().
-            populate({
+        const quizSets = await Quizset.find().lean();
+        return replaceMongoIdInArray(quizSets);
+    } catch (e) {
+        throw new Error(e);
+    }
+}
+
+export async function getQuizSetById(id) {
+    try {
+        const quizSet = await Quizset.findById(id)
+            .populate({
                 path: "quizIds",
                 model: Quiz,
             }).lean();
-        return replaceMongoIdInArray(quizSets);
+        return replaceMongoIdInObject(quizSet);
     } catch (e) {
         throw new Error(e);
     }
