23877d3930b1162db8fc7c85b7fee3f8887f7768
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2024-08-18 20:09:13 +0600

    courses page reaggange

--- app/(main)/courses/_components/FilterCourseMobile.jsx
+++ app/(main)/courses/_components/FilterCourseMobile.jsx
@@ -138,41 +138,6 @@ const FilterCourseMobile = () => {
                                     </ul>
                                 </AccordionContent>
                             </AccordionItem>
-                            {/* Price filter */}
-                            <AccordionItem value="price">
-                                <AccordionTrigger className="py-3 text-sm text-gray-400 hover:text-gray-500">
-                                    <span className="font-medium text-gray-900">Price</span>
-                                </AccordionTrigger>
-
-                                <AccordionContent className="pt-6 animate-none">
-                                    <ul className="space-y-4">
-                                        {PRICE_OPTIONS.map((option, optionIdx) => (
-                                            <li
-                                                key={option.value}
-                                                className="flex items-center"
-                                            >
-                                                <Checkbox
-                                                    type="checkbox"
-                                                    id={`price-${optionIdx}`}
-                                                    onCheckedChange={() => {
-                                                        applyArrayFilter({
-                                                            type: "price",
-                                                            value: option.value,
-                                                        });
-                                                    }}
-                                                    checked={filter.price.includes(option.value)}
-                                                />
-                                                <label
-                                                    htmlFor={`price-${optionIdx}`}
-                                                    className="ml-3 text-sm text-gray-600 cursor-pointer"
-                                                >
-                                                    {option.label}
-                                                </label>
-                                            </li>
-                                        ))}
-                                    </ul>
-                                </AccordionContent>
-                            </AccordionItem>
                         </Accordion>
                     </SheetHeader>
                 </SheetContent>
