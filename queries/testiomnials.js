2569f7074eb3ba5ea4569e7440d02d8f54648a17
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2024-08-26 00:20:10 +0600

    dbConnect before call testimonials

--- queries/testiomnials.js
+++ queries/testiomnials.js
@@ -1,7 +1,9 @@
 import { replaceMongoIdInArray } from "@/lib/convertData";
 import { Testimonial } from "@/model/testimonial-model";
+import { dbConnect } from "@/service/mongo";
 
 export async function getTestimonialsForCourse(courseId) {
+    await dbConnect();
     const testimonials = await Testimonial.find({ courseId: courseId }).lean();
     return replaceMongoIdInArray(testimonials);
 }
