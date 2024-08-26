9303529fa4851776425705e5802b014f21e10835
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2024-08-26 15:53:43 +0600

    instructor modal

--- app/(main)/courses/[id]/page.js
+++ app/(main)/courses/[id]/page.js
@@ -1,8 +1,6 @@
-import { replaceMongoIdInArray } from "@/lib/convertData";
 import { getCourseDetails } from "@/queries/courses";
 import CourseDetails from "./_components/CourseDetails";
 import CourseDetailsIntro from "./_components/CourseDetailsIntro";
-import Testimonials from "./_components/Testimonials";
 
 const SingleCoursePage = async ({ params: { id } }) => {
     const course = await getCourseDetails(id);
@@ -16,7 +14,7 @@ const SingleCoursePage = async ({ params: { id } }) => {
 
             <CourseDetails course={course} />
 
-            {course?.testimonials && <Testimonials testimonials={replaceMongoIdInArray(course?.testimonials)} />}
+            {/* {course?.testimonials && <Testimonials testimonials={replaceMongoIdInArray(course?.testimonials)} />} */}
 
             {/* <RelatedCourses /> */}
         </>
--- queries/courses.js
+++ queries/courses.js
@@ -7,12 +7,11 @@ import { Quizset } from "@/model/quizset-model";
 import { Quiz } from "@/model/quizzes-model";
 import { Testimonial } from "@/model/testimonial-model";
 import { User } from "@/model/user-model";
-import { dbConnect } from "@/service/mongo";
 import { getEnrollmentsForCourse } from "./enrollments";
 import { getTestimonialsForCourse } from "./testiomnials";
 
 export async function getCourseList() {
-    await dbConnect();
+    // await dbConnect();
     const courses = await Course.find({ active: true }).select(["title", "subtitle", "thumbnail", "modules", "price", "category", "instructor"]).populate({
         path: "category",
         model: Category
@@ -30,7 +29,7 @@ export async function getCourseList() {
 }
 
 export async function getCourseDetails(id) {
-    await dbConnect();
+    // await dbConnect();
     const course = await Course.findById(id)
         .populate({
             path: "category",
@@ -65,7 +64,7 @@ export async function getCourseDetails(id) {
 }
 
 export async function getCourseDetailsByInstructor(instructorId, expand) {
-    await dbConnect();
+    // await dbConnect();
     const publishedCourses = await Course.find({ instructor: instructorId, active: true }).lean();
 
     const enrollments = await Promise.all(
@@ -105,7 +104,7 @@ export async function getCourseDetailsByInstructor(instructorId, expand) {
     }, 0)) / totalTestimonials.length;
 
     if (expand) {
-        await dbConnect();
+        // await dbConnect();
         const allCourses = await Course.find({ instructor: instructorId }).lean();
         return {
             "courses": allCourses?.flat(),
@@ -125,7 +124,7 @@ export async function getCourseDetailsByInstructor(instructorId, expand) {
 
 export async function create(courseData) {
     try {
-        await dbConnect();
+        // await dbConnect();
         const course = await Course.create(courseData);
         return JSON.parse(JSON.stringify(course));
     } catch (err) {
